edabbee3bcd4259df93667f8604c776fb896210b
PoloBaca <thebigbaca@gmail.com>
2025-03-17 17:24:02 -0600

    commit march 17 2025

---
+++ Ultimate-Python/Introduction to Python/test.py
@@ -0,0 +1,70 @@
+import random
+"""
+a=4
+print(a)
+
+a=a+4
+
+a=a*6
+print(a)
+a=a/3
+print(a)
+
+b=a-1
+print(b)
+print(a,b)
+
+c=a
+print(a,c)
+c=c-1
+print(a,c)
+a=5
+print(a)
+print(type(a))
+a=5.5
+print(a)
+print(type(a))
+a=5
+print(a)
+a=a/3
+print(a, type(a))
+a = 4
+b = 3
+c = a/b
+print(c, type(c))
+
+c=a//b
+print(c, type(c))
+a='Book'
+print(a, type(a))
+b='Python'
+print(a,b)
+c=a+b
+print(c)
+a='10'
+b='70'
+c=a+b
+print(c)
+a=12
+b=4
+d=a>b
+print(d)
+print(type(d))
+a=5
+b=2
+c=int(a/b)
+print(c)
+print(type(c))
+a=5.65
+b=str(a)
+print(b, type(b))
+c=float(b)
+print(c, type(c))
+a=5.65
+print(isinstance(a, int))
+print(isinstance(a,str))
+"""
+upperLimit=1
+lowerLimit=50
+a=random.randint(lowerLimit, upperLimit)
+print(a)
--- Ultimate-Python/control-flujo/calculadora.py
+++ Ultimate-Python/control-flujo/calculadora.py
@@ -1,3 +1,6 @@
 print("Bienvenidos a la calculadora en Python!")
 print("Para salir escribe Salir")
 print("Las operaciones son suma, multi, div y resta")
+print("Ingrese un número:")
+
+n1 = float(input())
