e3f904b4e6e1c32e329d23ff77d2545a313b881b
PoloBaca <thebigbaca@gmail.com>
2025-03-20 21:24:05 -0600

    commit march 20 2025

---
+++ Ultimate-Python/Introduction to Python/example1.7.py
@@ -0,0 +1,5 @@
+#Python script
+#Reading text from a variable
+
+theText=input('Please type something and press enter:')
+print(theText)
---
+++ Ultimate-Python/Introduction to Python/example1.8.py
@@ -0,0 +1,15 @@
+#Python script
+#Reading an integer and storing it in an integer variable
+
+#userText=input('Please type an integer:')
+#a=int(userText)
+#print(a)
+#print(type(a))
+
+#a=int(input('Please type an integer:'))
+#print(a)
+#print(type(a))
+
+a=float(input('Please type a real number:'))
+print(a)
+print(type(a))
