56170729ea34e8f33974a5fc55f9e8f5ea9c2864
PoloBaca <thebigbaca@gmail.com>
2025-04-01 23:03:11 -0600

    commit April 01 2025

---
+++ Ultimate-Python/Introduction to Python/excercise1.11.py
@@ -0,0 +1,9 @@
+# Python script
+# Computing the difference between two numbers
+
+a = float(input('Enter the first number: '))
+b = float(input('Enter the second number: '))
+
+absDifference = abs(a - b)
+
+print(str(absDifference))
---
+++ Ultimate-Python/Introduction to Python/excercise1.12.py
@@ -0,0 +1,8 @@
+# Python script
+# Request the user to input a real number and shows its absolute value
+
+import math
+
+a = float(input('Please input a real number: '))
+
+print(f'The absolute value is: {abs(a):.2f}')
