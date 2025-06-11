6371cf7be6b9ce821bb776916eded239c11313c8
PoloBaca <thebigbaca@gmail.com>
2025-06-10 23:08:12 -0600

    commit Jun 10 2025 2

---
+++ Ultimate-Python/Introduction to Python #2/example2.4.2.py
@@ -0,0 +1,12 @@
+#Python script
+#Write an execute a script which request the user to input an integer number.
+#Then the script should determine whether the number is positive, negative, or zero.
+
+myInteger=int(input('Please input an integer number:'))
+
+if myInteger<0:
+    print('The number is negative.')
+elif myInteger>0:
+    print('The number is positive.')
+else:
+    print('The number is zero.')
