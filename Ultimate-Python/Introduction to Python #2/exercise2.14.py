11e4defc6009441b4bdf6348e18ff16fa32aead7
PoloBaca <thebigbaca@gmail.com>
2025-12-04 22:56:38 -0600

    Update Dec 4 2025

---
+++ Ultimate-Python/Introduction to Python #2/exercise2.14.py
@@ -0,0 +1,17 @@
+# Python Script
+
+# Write and execute a script which request the user to input two real numbers
+# corresponding to the weight (in kilos) and the height (in meters).
+# The script has to find out the Body Mass Index (BMI) of the data input by the user
+# Finally, the script should display a message indicating the category according to
+# the following rules:
+
+# Underweight: BMI under 18.5.
+# Normal weight: BMI from 18.5 to 25.
+# Overweight: BMI from 25 to 30.
+# Obese Class 1: BMI from 30 to 35.
+# Obese Class 2: BMI above 35.
+
+# Remark: The formulation to compute the BMI is BMI = weight / height 2
+# Example: If the weight and height input by the user ar e64 and 1.65, it must
+# display the message 'You are in the class: normal weight'.
