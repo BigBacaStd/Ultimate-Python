2be8da3d4d3a59607b99fd3c604c7bbb042973e9
PoloBaca <thebigbaca@gmail.com>
2026-01-29 21:56:12 -0600

    Update Jan 29 2026

--- Ultimate-Python/Introduction to Python #2/exercise2.15.py
+++ Ultimate-Python/Introduction to Python #2/exercise2.15.py
@@ -9,3 +9,15 @@ Example: If the weight and height input by the user are 87 and 1.70, it has to d
 input by the user are 57 and 1.82, it has to display the message 'Your weight should be between 61.28 kilos and 82.81 kilos. You should gain 4.28 kilos'
 
 """
+
+user_weight = float(input("Please enter weight in kilos: "))
+user_height = float(input("Please enter height in meters: "))
+
+bmi = user_weight / (user_height ** 2)
+weight = bmi * (user_height ** 2)
+
+min_weight = bmi * (user_height ** 2)
+max_weight = bmi * (user_height ** 2)
+
+print(min_weight)
+print(max_weight)
