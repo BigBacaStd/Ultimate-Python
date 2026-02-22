3102b5c3b0ae3a94f17ba8092d7afbb49ece5d2c
PoloBaca <thebigbaca@gmail.com>
2026-02-22 00:29:48 -0600

    Update feb 21 2026

---
+++ Python Crash/2.8-File_extensions.py
@@ -0,0 +1,7 @@
+#Python Script
+#2.8- File Extensions: Python has a removesuffix() method that works exactly
+# like removeprefix(). Assign the value 'python_notes.txt' to a variable called filename.
+# Then use the removesuffix() method to display the name without the file extension.
+
+filename = 'python_notes.txt'
+print(filename.removesuffix('.txt'))
