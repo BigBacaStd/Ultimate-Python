3dd2561e4f642309fbc2183341f36614e68eb282
PoloBaca <thebigbaca@gmail.com>
2026-02-22 23:07:35 -0600

    Update feb 22 2026

---
+++ Python Crash S3/bicycles.py
@@ -0,0 +1,13 @@
+#Python script
+# Lists - example of a list:
+
+bicycles = ['trek', 'cannondale', 'redline', 'specialized']
+print(bicycles[1])
+print(bicycles[3])
+print(bicycles[-1].title())
+print(bicycles[-2].title())
+print(bicycles[-3].title())
+print(bicycles[-4].title())
+
+message = f"My first bicycle was a {bicycles[0].title()}"
+print(message)
