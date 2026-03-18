171aeda611df19f156b11fff7abc95270bb9d97d
PoloBaca <thebigbaca@gmail.com>
2026-03-17 21:38:24 -0600

    Update March 17 2026

---
+++ Python Crash S4/magicians.py
@@ -0,0 +1,6 @@
+#Python Script
+#Loops
+
+magicians = ['Alice', 'David', 'Carolina']
+for magician in magicians:
+    print(magician)
---
+++ Python Crash S4/magicians2.py
@@ -0,0 +1,9 @@
+#Python Script
+#Loops
+
+magicians = ['Alice', 'David', 'Carolina']
+for magician in magicians:
+    print(f"{magician.title()}, that was a great trick!")
+    print(f"I can´t wait to see your next trick, {magician.title()}.\n")
+
+print("Thank you, everyone. That was a great magic show.")
