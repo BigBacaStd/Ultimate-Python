287814ec6bfd9a1cdd5d59e2a1075ba180b5bca7
PoloBaca <thebigbaca@gmail.com>
2026-04-22 23:01:18 -0600

    Update April 22 2026

---
+++ If statements S05/cars.py
@@ -0,0 +1,10 @@
+# Python script
+from plotly.data import carshare
+
+cars = ['audi', 'bmw', 'subaru', 'toyota']
+
+for car in cars:
+    if car == "bmw":
+        print(car.upper())
+    else:
+        print(car.title())
