ec2d46071fa43ad16fd6f3010cd407f9c3f29600
PoloBaca <thebigbaca@gmail.com>
2026-04-25 01:35:12 -0600

    Update April 25 2026

---
+++ If statements S05/5.1-conditional_test.py
@@ -0,0 +1,14 @@
+#Python script
+
+"""
+Write a series of conditional test. Print a statement
+describing each test and your prediction for the result of each test. Y
+"""
+from plotly.data import carshare
+
+car = 'subaru'
+print("is car == 'subaru'? I predict True.")
+print(car == 'subaru')
+
+print("\ncar == 'audi'? I predict False")
+print(car == 'audi')
---
+++ If statements S05/magic_number.py
@@ -0,0 +1,5 @@
+# Python script
+
+answer = 39
+if answer != 42:
+    print("That is not the correct answer. Please try again!")
