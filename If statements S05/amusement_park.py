366bd2f759b535e02d196a5f579733bdab038d33
PoloBaca <thebigbaca@gmail.com>
2026-05-11 20:50:48 -0600

    Update May 11 2026

---
+++ If statements S05/amusement_park.py
@@ -0,0 +1,21 @@
+# Python script
+"""
+Admission for anyone under age 4 is free.
+Admission for anyone between the ages of 4 and 18 is $25
+Admission for anyone age 18 or older is $40
+
+How can we use an if statement to determine a person´s admission rate?
+The following code tests for the age group of a person and then prints an
+admission price message.
+"""
+age = 25
+
+if age < 4:
+    print("Your admission cost is $0.")
+
+elif age < 18:
+    print("Your admission cost is $25.")
+
+else:
+    print("Your admission cost is $40")
+00
