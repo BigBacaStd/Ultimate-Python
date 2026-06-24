943cb3c7d817d8641cc696a2123d2d93e241d346
PoloBaca <thebigbaca@gmail.com>
2026-06-23 23:17:47 -0600

    Update Jun 23 2026

--- Dictionaries S06/favorite_language3.py
+++ Dictionaries S06/favorite_language3.py
@@ -14,3 +14,5 @@ for name in favorite_languages.keys():
     if name in friends:
         language = favorite_languages[name].title()
         print(f"\t{name.title()}, I see you love {language}!")
+    if 'erin' not in favorite_languages.keys():
+        print("Erin, please take our poll!")
---
+++ Dictionaries S06/favorite_language4.py
@@ -0,0 +1,11 @@
+# Python
+
+favorite_languages = {
+    'jen': 'python',
+    'sarah': 'c',
+    'edward': 'rust',
+    'phil': 'python',
+}
+
+for name in sorted(favorite_languages.keys()):
+    print(f"{name.title()}, thank you for taking the poll")
