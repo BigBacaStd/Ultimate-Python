f5ba2d14de22b426b198778b1d284c073bab7f07
PoloBaca <thebigbaca@gmail.com>
2026-07-08 23:12:41 -0600

    Update July 8 2026

--- input and while loops S07/7-4_Pizza_Toppings.py
+++ input and while loops S07/7-4_Pizza_Toppings.py
@@ -4,3 +4,14 @@
 Write a loop that prompts the user to enter a series of pizza toppings until they enter a 'quit' value.
 As they enter each topping, print a message saying you'll add that topping to their pizza.
 """
+prompt = "\nPlease enter your pizza toppings (Type 'quit' to finish.): "
+
+
+while True:
+    topping = input(prompt)
+
+
+    if topping.lower() == 'quit':
+        break
+    else:
+        print("Great choice! We will add it to your pizza!")
--- input and while loops S07/7-5_Movie_Tickets.py
+++ input and while loops S07/7-5_Movie_Tickets.py
@@ -5,3 +5,20 @@ A movie theater charges different ticket prices depending on a person's age. If 
 the ticket is free; if they are between 3 and 12, the ticket is $10; and if they are over age 12, the ticket is
 $15, Write a loop in which you ask user's their age and then tell them the cost of their movie ticket.
 """
+prompt = "\nPlease enter your age to check ticket cost(Type 'quit' to finish): "
+
+
+active = True
+while active:
+    age = input(prompt)
+    if age.lower() == 'quit':
+        active = False
+    else:
+        age = int(age)
+
+        if age < 3:
+            print("The ticket is free.")
+        elif 3 <= age < 12:
+            print("Your ticket is $10")
+        else:
+            print("Your ticket is $15.")
