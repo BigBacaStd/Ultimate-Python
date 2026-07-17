2d31758dab8ee9917038219305fa922000311002
PoloBaca <thebigbaca@gmail.com>
2026-07-16 22:42:42 -0600

    Update July 16 2026

---
+++ Functions S08/8-7_Album.py
@@ -0,0 +1,10 @@
+#Python script
+"""
+Exercise 8-7: Album
+	1.	Write a function called make_album() that builds a dictionary describing a music album.
+	2.	Set the parameters: The function should take in an artist name and an album title, and it should return a dictionary containing these two pieces of information.
+	3.	Test it out: Use the function to make three dictionaries representing different albums. Print each return value to show that the dictionaries are storing the album information correctly.
+	4.	Add an optional parameter: Use None to add an optional parameter to make_album() that allows you to store the number of songs on an album.
+	5.	Update the dictionary: If the calling line includes a value for the number of songs, add that value to the album’s dictionary.
+	6.	Test the optional parameter: Make at least one new function call that includes the number of songs on an album.
+"""
---
+++ Functions S08/8-8_User_albums.py
@@ -0,0 +1,10 @@
+#Python script
+
+"""
+Exercise 8-8: User Albums
+	1.	Start with your program from Exercise 8-7: You will be modifying or adding to the code you just wrote.
+	2.	Create a while loop: Write a loop that prompts users to enter an album’s artist and title.
+	3.	Call your function: Once you have that information from the user, call your make_album() function using the user's input.
+	4.	Print the result: Print the dictionary that is created by the function to show it is working correctly.
+	5.	Provide a way to quit: Be sure to include a quit value in the while loop so the user can stop entering albums when they are done.
+"""
