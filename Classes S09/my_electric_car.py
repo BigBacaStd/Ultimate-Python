cb1ced9f8fc80bdc26c430b5ce33cf0e71081cbd
PoloBaca <thebigbaca@gmail.com>
2026-08-13 23:01:33 -0600

    Update Aug 13 2026

---
+++ Classes S09/my_electric_car.py
@@ -0,0 +1,9 @@
+#Python script
+
+from electric_car import ElectricCar
+
+my_leaf = ElectricCar('nissan', 'leaf', 2024)
+print(my_leaf.get_descriptive_name())
+my_leaf.battery.describe_battery()
+my_leaf.battery.get_range()
+
